3c2a6a145eb4636fe89dc6ef09bcd66f69d788a7
Dvdson <dvdsoncomp@gmail.com>
2016-08-24 08:30:36 -0300

    consertado bugs da formula da linha e alterado formula da circonferencia

--- src/Bresenham.h
+++ src/Bresenham.h
@@ -5,86 +5,110 @@
 #ifndef BASEBALL_FIELD_CG_BRESENHAM_H
 #define BASEBALL_FIELD_CG_BRESENHAM_H
 
+#include <iostream>
 #include <GL/gl.h>
 #include <stdlib.h>
 
-void bresenhamLine(int x1, int y1, int x2, int y2){
+#define PI 3.14159265359
 
-    int dx,dy,x,y,d,incNE,incE, incX = 1 ,incY = 1;
-    float angle;
+void bresenhamLine(GLint x0, GLint y0, GLint x1, GLint y1){
 
-    //troca caso o vetor seja direcionado para a esquerda
-    if(x2 < x1){
-        int aux = x1;
-        x1 = x2;
-        x2 = aux;
-        aux = y1;
-        y1 = y2;
-        y2 = aux;
-    }
-
-    //initial variables
-    dx = x2 - x1;
-    dy = y2 - y1;
-
-    if(dy < 0) incY = -1;
-
-    if(dx >= abs(dy)) {
-        d = 2 * dy - dx;
-        incE = 2*dy;
-        incNE = 2*(dy - dx);
-    }else{
-        d = 2 * dx - dy;
-        incE = 2*dx;
-        incNE = 2*(dx - dy);
-    }
-
-
-    x = x1;
-    y = y1;
-
-    //first point and the sequentials
-    glVertex2i(x,y);
-    while(x < x2 || y < y2) {
-        if(d<=0) {
-            d = d + incE;
-
-            if(dx >= abs(dy)) x+=incX;
-            else y+= incY;
-
-        } else {
-            d = d + incNE;
-            x+=incX;
-            y+=incY;
+    int dx = abs (x1 - x0);
+    int dy = abs (y1 - y0);
+    int x, y;
+    if (dx >= dy) {
+        int d = 2*dy-dx;
+        int ds = 2*dy;
+        int dt = 2*(dy-dx);
+        if (x0 < x1) {
+            x = x0;
+            y = y0;
         }
-        glVertex2f(x,y);
+        else
+        {
+            x = x1;
+            y = y1;
+            x1 = x0;
+            y1 = y0;
+        }
+        glVertex2i (x, y);
+        while (x < x1)
+        {
+            if (d < 0)
+                d += ds;
+            else {
+                if (y < y1) {
+                    y++;
+                    d += dt;
+                }
+                else {
+                    y--;
+                    d += dt;
+                }
+            }
+            x++;
+            glVertex2i (x, y);
+        }
     }
+    else {
+        int d = 2*dx-dy;
+        int ds = 2*dx;
+        int dt = 2*(dx-dy);
+        if (y0 < y1) {
+            x = x0;
+            y = y0;
+        }
+        else {
+            x = x1;
+            y = y1;
+            y1 = y0;
+            x1 = x0;
+        }
+        glVertex2i (x, y);
+        while (y < y1)
+        {
+            if (d < 0)
+                d += ds;
+            else {
+                if (x > x1){
+                    x--;
+                    d += dt;
+                } else {
+                    x++;
+                    d += dt;
+                }
+            }
+            y++;
+            glVertex2f (x, y);
+        }
+    }
+
+
 
 }
 
-void bresenhamCicle(int Xo,int Yo, int r) {
+void bresenhamCicle(int Xo, int Yo, int r) {
 
-    int x,y;
-    float d;
 
-    //initial variables
-    x = 0;
-    y = r;
-    d = 5/4 - r;
-
-    glVertex2i(x+Xo,y+Yo);
-    while(y > x) {
-        if( d < 0) {
-            d = d + 2 * x + 3;
-            ++x;
-        } else {
-            d = d + 2 * (x - y) + 5;
-            ++x;
-            ++y;
+    int x=0,y=r,p=1-r;
+    while(x<=y){
+        //Here Transform each x,y coordinates by r pixels
+//        glVertex2i(Xo+y, Yo+x);//0-45
+//        glVertex2i(Xo+x, Yo+y);//45-90
+//        glVertex2i(Xo-x, Yo+y);//90-135
+//        glVertex2i(Xo-y, Yo+x);//135-180
+//        glVertex2i(Xo-y, Yo-x);//180-225
+        glVertex2i(Xo-x, Yo-y);//225-270
+        glVertex2i(Xo+x, Yo-y);//270-315
+//        glVertex2i(Xo+y, Yo-x);//315-360
+        if(p<0)
+            p=p+2*x+3;
+        else{
+            p=p+2*(x-y)+5;
+            y--;
         }
-        glVertex2i(x+Xo,y+Yo);
+        x++;
     }
-
 }
 
 
